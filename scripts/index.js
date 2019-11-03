59fac356fed0831c19e22c09b907726b769db9d8
Nandini Nayak <nandini.r.nayak@gmail.com>
2019-11-04 09:35:45 +1100

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,24 @@
+var anim= bodymovin.loadAnimation({
+  container: document.getElementById('bm'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'data.json'
+})
+
+// console.log("elem", animation.renderer.elements[0].updateDocumentData({ t: 'new text', s: 20 }, 0));
+// console.log("elem1", anim.renderer.elements[0]);
+
+// anim.renderer.elements[0].updateDocumentData({ t: 'something' })
+
+
+anim.addEventListener("DOMLoaded", function () {
+    console.log("BFEORE:", anim.renderer.elements[0].currentTextDocumentData.t);
+    anim.renderer.elements[0].updateDocumentData({
+        t: 'w'
+    });
+    anim.play();
+    console.log("AFTER:", anim.renderer.elements[0]);
+    anim.play();
+});
+
