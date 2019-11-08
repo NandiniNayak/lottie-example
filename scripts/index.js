1c7e3f33847325ac93be407d8521270a91d56cbb
Nandini Nayak <nandini.r.nayak@gmail.com>
2019-11-08 15:11:16 +1100

    json file changed

--- scripts/index.js
+++ scripts/index.js
@@ -3,20 +3,12 @@ var anim= bodymovin.loadAnimation({
   renderer: 'svg',
   loop: true,
   autoplay: true,
-  path: 'data.json'
+  path: 'text_demo.json'
 })
-
-// console.log("elem", animation.renderer.elements[0].updateDocumentData({ t: 'new text', s: 20 }, 0));
-// console.log("elem1", anim.renderer.elements[0]);
-
-// anim.renderer.elements[0].updateDocumentData({ t: 'something' })
-
 
 anim.addEventListener("DOMLoaded", function () {
     console.log("BFEORE:", anim.renderer.elements[0]);
-    anim.renderer.elements[0].updateDocumentData({
-        t: 'w'
-    });
+  anim.renderer.elements[0].updateDocumentData({ t: 'text data changed', s: 90 }, 0);
     anim.play();
     console.log("AFTER:", anim.renderer.elements[0]);
     anim.play();
