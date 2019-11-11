45d770cb769fafcc7bcb2637d9e1a033b406777d
Nandini Nayak <nandini.r.nayak@gmail.com>
2019-11-11 14:37:12 +1100

    working lottie code with dynamic input

--- scripts/index.js
+++ scripts/index.js
@@ -6,11 +6,32 @@ var anim= bodymovin.loadAnimation({
   path: 'text_demo.json'
 })
 
-anim.addEventListener("DOMLoaded", function () {
-    console.log("BFEORE:", anim.renderer.elements[0]);
-  anim.renderer.elements[0].updateDocumentData({ t: 'text data changed', s: 90 }, 0);
+// anim.addEventListener("DOMLoaded", function () {
+//     console.log("BFEORE:", anim.renderer.elements[0]);
+//   anim.renderer.elements[0].updateDocumentData({ t: 'text data changed', s: 90 }, 0);
+//     anim.play();
+//     console.log("AFTER:", anim.renderer.elements[0]);
+//     anim.play();
+// });
+
+function playAnimation(jsonFileName){
+    var anim= bodymovin.loadAnimation({
+    container: document.getElementById('newAnimation'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: jsonFileName
+  })
+
+  anim.addEventListener("DOMLoaded", function () {
+    var newText = document.getElementById("input").value
+    console.log("dom", newText)
+    console.log("BEFORE:", anim.renderer.elements[0]);
+    anim.renderer.elements[0].updateDocumentData({ t: newText, s: 90 }, 0);
     anim.play();
     console.log("AFTER:", anim.renderer.elements[0]);
     anim.play();
-});
+  });
+
+}
 
